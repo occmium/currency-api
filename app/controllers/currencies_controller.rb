84b6970f661b37026bdf25d77d0804bbbd996a3c
occmium <occkart@yahoo.com>
2020-01-26 13:04:19 +0600

    Add model ApiKey for authorization

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,4 +1,7 @@
 class CurrenciesController < ApplicationController
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+  before_action :restrict_access
+
   def index
     currencies = Currency.paginate(page: params[:page], per_page: 10)
     render json: currencies
@@ -8,4 +11,12 @@ class CurrenciesController < ApplicationController
     currency = Currency.find(params[:id])
     render json: currency
   end
+
+  private
+
+    def restrict_access
+      authenticate_or_request_with_http_token do |token, _|
+        ApiKey.exists?(access_token: token)
+      end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_26_020905) do
+ActiveRecord::Schema.define(version: 2020_01_26_051700) do
+
+  create_table "api_keys", force: :cascade do |t|
+    t.string "access_token", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "currencies", force: :cascade do |t|
     t.string "name"
